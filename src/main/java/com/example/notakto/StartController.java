ca784535feb1bd5d03270b1c87ae018390eeb55b
Fymek <Fymek01@gmail.com>
2022-05-18 23:12:58 +0200

    dodanie ekranu startowego i grafik

---
+++ src/main/java/com/example/notakto/StartController.java
@@ -0,0 +1,33 @@
+package com.example.notakto;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class StartController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
+    //metoda do zmiany na Rules
+//    public void switchToScene1(ActionEvent event) throws IOException {
+//        root = FXMLLoader.load(getClass().getResource("Rules.fxml"));
+//        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+//        scene = new Scene(root);
+//        stage.setScene(scene);
+//        stage.show();
+//    }
+    public void switchToScene2(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
